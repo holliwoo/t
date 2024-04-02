bce8f831c735b1d2bc4666091ebe895707d2812e
holliwoo <165030469+holliwoo@users.noreply.github.com>
2024-04-02 22:44:30 +1200

    Update random.ts

--- src/lib/random.ts
+++ src/lib/random.ts
@@ -2,7 +2,7 @@ const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
 export function getRandomId(): string {
   return Array.from({ length: 20 }).reduce(
-    (acc: string) => acc + CHARS[~~(Math.random() * CHARS.length)],
+    (acc: string, _: any) => acc + CHARS[~~(Math.random() * CHARS.length)],
     ''
   );
 }
